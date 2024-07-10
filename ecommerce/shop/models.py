a003df47d7175908c662a2eaf41397e213e3d9b8
surabhix <surabhix16@email.com>
2024-07-10 20:33:18 +0530

    css for item_details

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -13,7 +13,7 @@ class Item(models.Model):
     description = models.TextField()
     image = models.ImageField(upload_to="items/")
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    price = models.FloatField(default=0.0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
