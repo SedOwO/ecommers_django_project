aef70cac7eca70ed943fe61b276813107c68ce76
skia <sairagav2003@gmail.com>
2024-07-10 21:59:45 +0530

    beta release

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -17,23 +17,24 @@ def item_detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     return render(request, "shop/item_detail.html", {"item": item})
 
+
 def cart_detail(request):
     cart = request.session.get("cart", {})
     items = Item.objects.filter(id__in=cart.keys())
     total_amount = sum(item.price * cart[str(item.id)] for item in items)
-    
+
     # Create a list of tuples (item, count)
     cart_items = [(item, cart[str(item.id)]) for item in items]
-    
-    return render(
-        request, "shop/cart_detail.html", {"cart_items": cart_items, "total_amount": total_amount}
-    )
 
+    return render(
+        request,
+        "shop/cart_detail.html",
+        {"cart_items": cart_items, "total_amount": total_amount},
+    )
 
 
 def checkout(request):
     return render(request, "shop/checkout.html")
-
 
 
 def add_to_cart(request, pk):
@@ -46,21 +47,24 @@ def add_to_cart(request, pk):
     request.session["cart"] = cart
     return redirect("cart_detail")
 
+
 def reduce_quantity(request, item_id):
-    item = get_object_or_404(Item, id=item_id)
-    
-    # Assuming you have a session-based cart management system
-    if item.id in request.session.get('cart', {}):
-        request.session['cart'][item.id] -= 1
-        if request.session['cart'][item.id] <= -1:
-            del request.session['cart'][item.id]
-    
-    return redirect('cart_detail')  
+    item = get_object_or_404(Item, pk=item_id)
+    cart = request.session.get("cart", {})
+    item_id_str = str(item.id)
+    if item_id_str in cart:
+        cart[item_id_str] -= 1
+        if cart[item_id_str] <= 0:
+            del cart[item_id_str]
+    request.session["cart"] = cart
+    return redirect("cart_detail")
+
+
 def remove_item(request, item_id):
-    item = get_object_or_404(Item, id=item_id)
-    
-    # Assuming you have a session-based cart management system
-    if item.id in request.session and item_id in request.session['cart']:
-        del request.session['cart'][item.id]
-    
-    return redirect('cart_detail')  
+    item = get_object_or_404(Item, pk=item_id)
+    cart = request.session.get("cart", {})
+    item_id_str = str(item.id)
+    if item_id_str in cart:
+        del cart[item_id_str]
+    request.session["cart"] = cart
+    return redirect("cart_detail")
